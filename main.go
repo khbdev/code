579f979e571918e6b51388236b5a738761c0dbde
Azizbekweb098 <webcoderazizbek@gmail.com>
2026-01-29 14:22:01 +0500

    gorutina-wg-waitGroup

--- main.go
+++ main.go
@@ -1,61 +1,27 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
+	"fmt"
 	"sync"
 )
 
-type User struct {
-	ID int 	`json:"id"`
-	Name string `json:"name"`
+func GoruOne(){
+	fmt.Println("Salom")
 }
 
-
-var (
-	users = []User{}
-	nextId = 1
-	my sync.Mutex
-)
-
-
-func GetUsers(w http.ResponseWriter, r *http.Request){
-	my.Lock()
-	defer my.Unlock()
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
-}
-
-func PostUsers(w http.ResponseWriter, r *http.Request){
-	var u User
-	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-		http.Error(w, "Invalid Create User", http.StatusBadRequest)
-		return
-	}
-
-	my.Lock()
-	u.ID = nextId
-	nextId++
-	users = append(users, u)
-	my.Unlock()
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
-
-}
-
-func UserHandler(w http.ResponseWriter, r *http.Request){
-	switch r.Method{
-	case http.MethodGet:
-		GetUsers(w, r)
-	case http.MethodPost:
-		PostUsers(w, r)
-	}
-}
 
 func main(){
-	http.HandleFunc("/users", UserHandler)
 
-	http.ListenAndServe(":8081", nil)
+var wg sync.WaitGroup
+
+wg.Add(1)
+go func() {
+	wg.Done()
+	GoruOne()
+}()
+
+wg.Wait()
+
+
+
 }
