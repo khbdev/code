d9f57bd899262d37946efb836d98f7903f095032
Azizbekweb098 <webcoderazizbek@gmail.com>
2026-01-30 17:28:13 +0500

    gorutina

--- main.go
+++ main.go
@@ -2,33 +2,44 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
+
 )
 
 
-func worker(id int, jobs <-chan int){
- for  job := range jobs {
-		fmt.Printf("Worker id %d, job %d ni ishlayabdi \n", id, job)
-	time.Sleep(time.Second)
- }
+func worker(id int, jobs <- chan int, result chan<- int, wg *sync.WaitGroup) {
+	for  job := range jobs {
+			fmt.Printf("Worker %d job %d ni ishlayapdi\n", id, job)
+		
+		result <- job * 2 
+		 wg.Done()
+	}
 }
 
-
 func main(){
-	jobs := make(chan int)
+	jobs := make(chan int, 10)
+	result := make(chan int, 10)
+	value := 10
+
+var wg sync.WaitGroup
 
 	for i := 1; i <= 3; i++ {
 		go func(id int) {
-			worker(id, jobs)
+			worker(id, jobs, result, &wg)
 		}(i)
 	}
 
-	for j := 1; j <= 10; j++ {
-		jobs <- j
+	
+
+	for i := 1; i <= value; i++ {
+		wg.Add(1)
+		jobs <- i
 	}
 	close(jobs)
 
-	time.Sleep(5 *time.Second)
+	for i := 1; i <= value; i++ {
+	fmt.Println("Malumot", <-result)
+	}
+	wg.Wait()
+	close(result)
 }
-
-
