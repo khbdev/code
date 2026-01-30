33c9dfc2bfcbb817bae53cc080f019f69d65d7a9
Azizbekweb098 <webcoderazizbek@gmail.com>
2026-01-30 16:50:49 +0500

    worker-pool

--- main.go
+++ main.go
@@ -7,23 +7,24 @@ import (
 
 
 func worker(id int, jobs <-chan int){
-	fmt.Printf("Worker id %d, job %d ni ishlayabdi", id, <-jobs)
-	time.Sleep(1  * time.Second)
+ for  job := range jobs {
+		fmt.Printf("Worker id %d, job %d ni ishlayabdi \n", id, job)
+	time.Sleep(time.Second)
+ }
 }
 
 
 func main(){
 	jobs := make(chan int)
 
-	for i := 1; i < 3; i++ {
-		go func() {
-			worker(i, jobs)
-		}()
+	for i := 1; i <= 3; i++ {
+		go worker(i, jobs)
 	}
 
-	for i := 1; i <= 10; i++ {
-		jobs <- i
+	for j := 1; j <= 10; j++ {
+		jobs <- j
 	}
+	close(jobs)
 
 	time.Sleep(5 *time.Second)
 }
