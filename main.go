9df7c7dd11d13672fa4963b6afd3ceda223fd66d
Azizbekweb098 <webcoderazizbek@gmail.com>
2026-02-02 18:14:28 +0500

    mutex

--- main.go
+++ main.go
@@ -3,43 +3,29 @@ package main
 import (
 	"fmt"
 	"sync"
-
 )
 
 
-func worker(id int, jobs <- chan int, result chan<- int, wg *sync.WaitGroup) {
-	for  job := range jobs {
-			fmt.Printf("Worker %d job %d ni ishlayapdi\n", id, job)
-		
-		result <- job * 2 
-		 wg.Done()
-	}
+var (
+	number int
+	mu sync.Mutex
+	wg sync.WaitGroup
+)
+
+
+func incrament(){
+	defer wg.Done()
+	mu.Lock()
+	number++
+	mu.Unlock()
 }
 
+
 func main(){
-	jobs := make(chan int, 10)
-	result := make(chan int, 10)
-	value := 10
-
-var wg sync.WaitGroup
-
-	for i := 1; i <= 3; i++ {
-		go func(id int) {
-			worker(id, jobs, result, &wg)
-		}(i)
-	}
-
-	
-
-	for i := 1; i <= value; i++ {
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
-		jobs <- i
-	}
-	close(jobs)
-
-	for i := 1; i <= value; i++ {
-	fmt.Println("Malumot", <-result)
+		go incrament()
 	}
 	wg.Wait()
-	close(result)
+	fmt.Println(number)
 }
