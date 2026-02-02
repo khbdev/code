ee75807fb1d8a1462a28b800bf66d1d4f34ef636
Azizbekweb098 <webcoderazizbek@gmail.com>
2026-02-02 18:29:56 +0500

    mutex-fan-out

--- main.go
+++ main.go
@@ -6,26 +6,45 @@ import (
 )
 
 
+
 var (
-	number int
 	mu sync.Mutex
-	wg sync.WaitGroup
 )
 
 
-func incrament(){
+func worker(id int, jobs <- chan int, wg *sync.WaitGroup){
 	defer wg.Done()
-	mu.Lock()
-	number++
+
+mu.Lock()
+	for  job := range jobs {
+		fmt.Printf("gorutina id %d, worker id %d\n", id, job)
+	}
 	mu.Unlock()
 }
 
 
 func main(){
-	for i := 0; i < 1000; i++ {
+	jobs :=  make(chan int, 10)
+
+
+   var wg sync.WaitGroup
+
+	for i := 1; i <= 3; i++ {
 		wg.Add(1)
-		go incrament()
+		go func(id int) {
+				
+			worker(id, jobs, &wg)
+		}(i)
 	}
+
+	for i := 1; i <= 10; i++ {
+	
+jobs <- i
+	}
+	close(jobs)
+
 	wg.Wait()
-	fmt.Println(number)
+
+	
+
 }
