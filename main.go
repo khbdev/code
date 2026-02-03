997a44dbe0aa1a05c3b99e8b89a4e62fd1edc260
Azizbekweb098 <webcoderazizbek@gmail.com>
2026-02-03 23:18:13 +0500

    user-crud-cli-one-version

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 
 
@@ -13,24 +16,39 @@ type User struct {
 var (
 	users []User
 	lastID = 0
+	wg sync.WaitGroup
+	mu sync.Mutex
 )
 
-func CreateUser(name string) User {
+func CreateUser(name string, wg *sync.WaitGroup) User {
+	wg.Done()
+	mu.Lock()
 	lastID++
 	user := User {
       ID: lastID,
 	  Name: name,
 	}
+	users = append(users, user)
+	
+	mu.Unlock()
 	return  user
 }
 
 func main(){
 	var name string
-
+	
+  
 	for i := 1; i <= 5; i++ {
 		fmt.Print("Name: ", )
 	fmt.Scanln(&name)
 	}
 
-	
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go func(id int) {
+			fmt.Print("Name: ", )
+	fmt.Scanln(&name)
+		}(i)
+	}
+	wg.Wait()
 }
