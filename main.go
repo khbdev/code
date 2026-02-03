31488b770462062fda120c8de0929d693e80a78c
Azizbekweb098 <webcoderazizbek@gmail.com>
2026-02-03 23:10:16 +0500

    user-crud-cli-one-version

--- main.go
+++ main.go
@@ -1,50 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
 
+
+type User struct {
+	ID int 
+	Name string
+}
 
 
 var (
-	mu sync.Mutex
+	users []User
+	lastID = 0
 )
 
-
-func worker(id int, jobs <- chan int, wg *sync.WaitGroup){
-	defer wg.Done()
-
-mu.Lock()
-	for  job := range jobs {
-		fmt.Printf("gorutina id %d, worker id %d\n", id, job)
+func CreateUser(name string) User {
+	lastID++
+	user := User {
+      ID: lastID,
+	  Name: name,
 	}
-	mu.Unlock()
+	return  user
 }
 
-
-func main(){
-	jobs :=  make(chan int, 10)
-
-
-   var wg sync.WaitGroup
-
-	for i := 1; i <= 3; i++ {
-		wg.Add(1)
-		go func(id int) {
-				
-			worker(id, jobs, &wg)
-		}(i)
-	}
-
-	for i := 1; i <= 10; i++ {
-	
-jobs <- i
-	}
-	close(jobs)
-
-	wg.Wait()
-
-	
-
-}
