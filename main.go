4c91522d79e2f8786d4f2c3064dce7608bb01201
Azizbek <webcoderazizbek@gmail.com>
2026-02-10 23:34:46 -0200

    fail

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+type User struct {
+	Name string
+	Age  int
+}
+
+var (
+	users  = []User
+	lastId = 0
+)
+
+func CreateUser(name string, age int) {
+	lastId++
+	user := User{
+		Name: name,
+		Age:  age,
+	}
+	users = append(users, user)
+
+}
+
+func GetUsers() User {
+	return users
+}
+func getByIdUsers(id int) User {
+	user := users[id]
+
+	return user
+}
 
 func main() {
-	fmt.Println("Salom ")
+	CreateUser("Gopher", 17)
+	GetUsers()
+	getByIdUsers(1)
+
 }
