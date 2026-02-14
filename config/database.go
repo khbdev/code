3ae439ddcf04ee251151cc5a2ee012d02bccda79
Azizbek <webcoderazizbek@gmail.com>
2026-02-14 21:13:55 -0200

    code

--- config/database.go
+++ config/database.go
@@ -18,5 +18,15 @@ type Postgress struct {
 }
 
 func ConfigPostgress(cfg Postgress) (*sql.DB, error) {
-	dsn := fmt.Sprintf("host=%s")
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBname, cfg.SSLMode)
+
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return  nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return  nil, err
+	}
+	return db, nil
 }
