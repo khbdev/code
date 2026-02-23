f2a06585c8d1164cda8834fc7498ec14ddfd181e
Azizbek <webcoderazizbek@gmail.com>
2026-02-23 16:41:49 -0200

    steack

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
 package main
 
-import (
-	// "fmt"
-	"time"
-)
-
-
-func Worker(name string){
-	// for i := 1; i <= 3; i++ {
-	//   fmt.Println(name, "Workering", i)
-	//   time.Sleep(time.Second)
-	// }
-	panic("boom")
-}
-
+import "fmt"
 
 func main(){
-	go Worker("Thread-1")
-	go Worker("Thread-2")
-
-	time.Sleep(4 * time.Second)
+	x := 10
+	fmt.Println(x)
 }
