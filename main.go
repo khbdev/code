4c9e6a5eb3f8e03b38fc8599766a9f1418f1bfed
Azizbek <webcoderazizbek@gmail.com>
2026-02-24 06:22:17 -0200

    procress

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
-package code
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
 
 
 
-func Value() int {
-	x := 10
-	return  x
+func Worker(id int, wg *sync.WaitGroup){
+	defer wg.Done()
+
+for i := 0; i < 3; i++ {
+	fmt.Println("Worker", id, "iteration", i)
+		time.Sleep(200 * time.Millisecond)
+}
 }
 
-func main(){
-	Value()
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	fmt.Println("Cpu Cores:", runtime.NumCPU())
+
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go Worker(i, &wg)
+	}
+
+	wg.Wait()
 }
