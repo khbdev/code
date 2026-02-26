e603ab7d2d980889ef0c7a802c5ae94ee9ee184b
Azizbek <webcoderazizbek@gmail.com>
2026-02-25 22:19:48 -0200

    code

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
+ "fmt"
+ "sync"
+ "time"
 )
 
-func main(){
-	cmd := exec.Command("bash", "-ls", "ls", "la")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Println("error", err)
-		return
-	}
-	fmt.Println(string(out))
+func main() {
+ firstTest()
+ // secondTest()
+}
+
+// ---------------------------------------------------------------------------------------
+
+// order count 1_000_000 marta qo'shib chiqqanda boshqa son chiqmoqda.
+// shuni to'gri increment qiladigan qilish kerak.
+// berilgan vaqt 5-10 daqiqa
+func firstTest() {
+ var orderCount int
+ for i := 0; i < 1_000_000; i++ {
+  go func() {
+   orderCount++
+  }()
+ }
+
+ fmt.Printf("order count: %d\n", orderCount)
+}
+
+// ---------------------------------------------------------------------------------------
+
+// agar request 3 soniadan koproq vaqt oladigan bo'lsa, biz kutmay, panic qilishimiz kerak
+// berilgan vaqt 5-10 daqiqa
+func secondTest() {
+ response := simulateRequest()
+ fmt.Printf("received response: %d\n", response)
+}
+
+func simulateRequest() int {
+ time.Sleep(time.Second * 5)
+ return 1
 }
