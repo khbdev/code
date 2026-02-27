ff7f6e3fe010c7232df478dd3781e63cc8476ba1
Azizbek <webcoderazizbek@gmail.com>
2026-02-27 21:17:26 -0200

    coee

--- main.go
+++ main.go
@@ -3,51 +3,39 @@ package main
 import (
 	"fmt"
 	"sync"
-
-	"time"
 )
- var mu sync.Mutex
- var wy sync.WaitGroup
 
-func main() {
- firstTest()
- // secondTest()
- 
-}
 
-// ---------------------------------------------------------------------------------------
+func main(){
+	total := 1_000_000
+	worker := 10
 
-// order count 1_000_000 marta qo'shib chiqqanda boshqa son chiqmoqda.
-// shuni to'gri increment qiladigan qilish kerak.
-// berilgan vaqt 5-10 daqiqa
-func firstTest() {
- var orderCount int
+	chunk := total / worker
 
- for i := 0; i < 1_000_000; i++ {
-	wy.Add(1)
-  go func() {
-	mu.Lock()
-   orderCount++
-   mu.Unlock()
-   wy.Done()
-  }()
- }
-  wy.Wait()
+	results := make([]int, worker)
 
- fmt.Printf("order count: %d\n", orderCount)
+	var wg sync.WaitGroup
 
-}
+	for i := 0; i < worker; i++ {
+		wg.Add(1)
 
-// ---------------------------------------------------------------------------------------
+		go func(id int) {
+			defer wg.Done()
+			start := id * chunk
+			end := start + chunk
 
-// agar request 3 soniadan koproq vaqt oladigan bo'lsa, biz kutmay, panic qilishimiz kerak
-// berilgan vaqt 5-10 daqiqa
-func secondTest() {
- response := simulateRequest()
- fmt.Printf("received response: %d\n", response)
-}
+			local := 0
+			for i := start; i < end; i++ {
+				local++
+			}
+			results[id] = local
+		}(i)
+	}
+	wg.Wait()
 
-func simulateRequest() int {
- time.Sleep(time.Second * 5)
- return 1
+	sum := 0
+	for _, v := range results {
+		sum+=v
+	}
+	fmt.Println("Natija: ", sum)
 }
