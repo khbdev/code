2d2cca3a60bdf0b08b929b2a0aa02fd8b71b5089
Azizbek <webcoderazizbek@gmail.com>
2026-03-10 16:42:23 -0200

    main

--- main.go
+++ main.go
@@ -1,58 +1,48 @@
 package main
 
 import (
- "fmt"
-
- "time"
+	"fmt"
+	"sync"
 )
 
-func main() {
- //firstTest()
-  secondTest()
+
+
+func worker(id int, jobs <- chan int, result chan <- int, wg *sync.WaitGroup){
+	defer wg.Done()
+	for  v := range jobs {
+		fmt.Printf("Worker Id %d, job %d\n", id, v)
+		result <- 2 *  v
+	}
 }
 
-// ---------------------------------------------------------------------------------------
-
-// order count 1_000_000 marta qo'shib chiqqanda boshqa son chiqmoqda.
-// shuni to'gri increment qiladigan qilish kerak.
-// berilgan vaqt 5-10 daqiqa
-func firstTest() {
- var orderCount int
- for i := 0; i < 1_000_000; i++ {
-  go func() {
-   orderCount++
-  }()
- }
-
- fmt.Printf("order count: %d\n", orderCount)
-}
-
-// ---------------------------------------------------------------------------------------
-
-// agar request 3 soniadan koproq vaqt oladigan bo'lsa, biz kutmay, panic qilishimiz kerak
-// berilgan vaqt 5-10 daqiqa
-func secondTest() {
- ctx, cancel := context.WithTimeOut(context.Backround(), 5 *time.Second)
- defer cancel()
- 
- result := make(chan int)
- 
+func main(){
+	jobs := make(chan int, 10)
+	result := make(chan int, 10)
 
 
- go func() {
-	 result <- simulateRequest()
- }()
- 
- select {
- case res := <-result:
- fmt.Println("malumot: ", res)
- case <-ctx.Done():
- fmt.Println("vaqt yetmadi")
- }
- 
-}
+	jobsList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var wg sync.WaitGroup
 
-func simulateRequest() int {
- time.Sleep(time.Second * 5)
- return 1
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go func(id int) {
+			worker(id, jobs, result, &wg)
+		}(i)
+	}
+
+	for _, v := range jobsList {
+	 jobs <- v
+	}
+	close(jobs)
+
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+
+	for  res := range result {
+		fmt.Println("Natija: ", res)
+	}
+
+	
 }
