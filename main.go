7a9355689d0808e482c3e7f529dc3e4853d3c455
Azizbek <webcoderazizbek@gmail.com>
2026-03-18 16:54:59 -0200

    code

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
+	"time"
 )
 
 
 func main(){
 	var wg sync.WaitGroup
 
-
+fmt.Println("Boshlanishda Gorutinlar soni: ", runtime.NumGoroutine())
 for i := 0; i < 1000; i++ {
 	wg.Add(1)
 
@@ -17,10 +19,13 @@ for i := 0; i < 1000; i++ {
 		defer wg.Done()
 		fmt.Printf("gorutina id  %d\n", id)
 	}(i)
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("Ishlayotganda goroutine soni:", runtime.NumGoroutine())
 
 	wg.Wait()
 
 
 }
 	fmt.Println("Gorutinlar ishini tugatdi")
+		fmt.Println("Tugagandan keyin goroutine soni:", runtime.NumGoroutine())
 }
