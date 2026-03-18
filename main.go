b85ce5cd6bb1d99d06018fc994c38d9949dd2ddc
Azizbek <webcoderazizbek@gmail.com>
2026-03-18 16:52:50 -0200

    code

--- main.go
+++ main.go
@@ -1,2 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+
+func main(){
+	var wg sync.WaitGroup
+
+
+for i := 0; i < 1000; i++ {
+	wg.Add(1)
+
+	go func(id int) {
+		defer wg.Done()
+		fmt.Printf("gorutina id  %d\n", id)
+	}(i)
+
+	wg.Wait()
+
+
+}
+	fmt.Println("Gorutinlar ishini tugatdi")
+}
