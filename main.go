6bb5e5f6fca484e82f07364acf12f4ed6df3d0aa
Azizbek <webcoderazizbek@gmail.com>
2026-04-15 15:35:12 +0500

    code

--- main.go
+++ main.go
@@ -1,57 +1,22 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-)
+import "fmt"
 
-
-
-func main(){
-mux := http.NewServeMux()
-
-mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(w, "Salom")
-})
-
-mux.HandleFunc("/hoyy", func(w http.ResponseWriter, r *http.Request) {
-  log.Printf("Request keldi")
-  time.Sleep(11 * time.Second)
-   log.Printf("Request tugadi")
-   fmt.Println(w, "Request tugadi")
-})
-
-
-sigCh := make(chan os.Signal, 1)
-
-signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-
-server := &http.Server{
-    Addr: ":8083",
-    Handler: mux,
+type User struct {
+	Name string
+	Age int
 }
 
 
-go func() {
-    fmt.Println("Server Running :8083")
-    server.ListenAndServe()
-}()
+var users []User
 
+func main(){
+for i := 0; i < 1_000_000_000; i++ {
+		users = append(users, User{Name: "Azizbek", Age: 17})
+}
 
-sig := <-sigCh
-fmt.Println("Signal keldi", sig)
-
-ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-defer cancel()
-
-server.Shutdown(ctx)
-server.ListenAndServe()
-
-fmt.Println("Server ochdi")
+for _, v := range users {
+	fmt.Println(v.Name)
+	fmt.Println(v.Age)
+}
 }
